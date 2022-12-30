6141767ffb6145e828710b6444de7b8ea6d9ea0f
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2022-12-30 23:14:00 +0300

    Feat: add save json feature

--- app/Jobs/Insert/InsertPoemJob.php
+++ app/Jobs/Insert/InsertPoemJob.php
@@ -4,19 +4,28 @@ namespace App\Jobs\Insert;
 
 use App\Jobs\Parse\PoemParse;
 use App\Models\Poems;
+use Illuminate\Support\Facades\Storage;
 
 class InsertPoemJob
 {
+    public const FILE_NAME = 'poems.json';
+
     public function __construct(private PoemParse $data)
     {
     }
 
     public function insert()
     {
-        return Poems::query()->create([
+        $serializedData = [
             'title' => $this->data->getTitle(),
             'body' => $this->data->getBody(),
             'writer' => $this->data->getWriter()
-        ]);
+        ];
+        if (config('antoloji.saveAsJson')) {
+            $json = json_decode(Storage::get(self::FILE_NAME) ?? "{}", true);
+            $json[] = $serializedData;
+            Storage::disk()->put(self::FILE_NAME, json_encode($json));
+        }
+        return Poems::query()->create($serializedData);
     }
 }
